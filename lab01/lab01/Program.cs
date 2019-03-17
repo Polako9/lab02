82ed4a1f344931012422ac2934336776e90fdf9f
Polako93 <48654426+Polako93@users.noreply.github.com>
2019-03-17 19:26:20 +0100

    Wersja 01

---
+++ lab01/lab01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program na powitanie");
+            Console.ReadKey();
+            Console.Write("Podaj imię : ");
+            string imie;
+            imie = Console.ReadLine();
+            Console.Write("Podaj nazwisko : ");
+            string nazwisko;
+            nazwisko = Console.ReadLine();
+            Console.WriteLine("Witaj " + imie + " " + nazwisko);
+            Console.WriteLine("Witaj {0} {1}. Czy Pan {0} rzeczywiście nazywa się {1}?", imie, nazwisko);
+
+            Console.Write("Podaj wiek: ");
+            int wiek = Convert.ToInt32(Console.ReadLine());
+            if (wiek < 67 )
+            {
+                Console.WriteLine(" do emerytury zostało ci {0} lat ", 67 - wiek);
+            }
+            else
+            {
+                Console.WriteLine(" jestes emerytem");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
