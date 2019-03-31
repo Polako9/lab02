60a4c39b6dcc32d1faf8a6f3fc1d08223eefec2d
Polako93 <48654426+Polako93@users.noreply.github.com>
2019-03-31 19:28:48 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,7 +14,49 @@ namespace ConsoleApp1
         static void Space() => Console.Write(" ");
         static void SpaceLn() => Console.WriteLine(" ");
         static void NewLine() => Console.WriteLine();
+        static void LiteraX(int n)
+        {
+            if (n < 3) throw new ArgumentException("zbyt mały rozmiar");
+            if (n % 2 == 0) n = n + 1;
 
+            //górna połówka
+            for (int i = 0; i < n / 2; i++)
+            {
+                for (int j = 0; j < i; j++)
+                    Space();
+                Star();
+                for (int j = 0; j < n - 2 - 2 * i; j++)
+                    Space();
+                StarLn();
+            }
+
+           
+          
+
+        }
+        static void LiteraY(int n)
+        {
+            if (n < 3) throw new ArgumentException("zbyt mały rozmiar");
+            if (n % 2 == 0) n = n + 1;
+
+            //górna połówka
+            for (int i = 0; i < n / 2 ; i++)
+            {
+                for (int j = 0; j < n - 2 - 2 -2 -2*i; j++)
+
+                    Space();
+                StarLn();
+
+                
+
+
+            }
+
+
+
+
+        }
+        /*
         public static void Prostokat(int n, int m)
         {
             for (int i = 0; i < n; i++)
@@ -38,10 +80,12 @@ namespace ConsoleApp1
             }
             NewLine();
         }
-
+        */
         public static void Main(string[] args)
         {
-            Prostokat(5, 7);
+            LiteraX(8);
+  
+            LiteraY(8);
         }
     }
 }
